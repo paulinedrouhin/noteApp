09b9143ef0739b6ddebaee6def57e01d08e2ba40
Pauline Drouhin <pauline.drouhin@gmail.com>
2020-01-25 11:11:18 +0100

    project initialization

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import express, {Application, Request, Response, NextFunction} from "express"
+const app : Application = express()
+
+app.get('/', (req: Request,res: Response, next: NextFunction) => {
+    res.send('hello')
+})
+
+app.listen(4000, () => console.log('server running'))
