bc47673cc465a240135fe5d4a0f9d1b123bdc7de
Pauline Drouhin <pauline.drouhin@gmail.com>
2020-01-26 22:49:44 +0100

    endpoints documentation with swagger and endpoint /api-docs created to access it

--- src/controller/note.ts
+++ src/controller/note.ts
@@ -2,21 +2,24 @@ import { Request, Response, Router } from "express"
 const router: Router = Router()
 import * as type from '../model/types'
 import * as promise from "../model/promises"
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
+router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 // endpoint that allows a client to create an author // 
 // expected parameters in body : name //
-router.post('/', async (req: Request, res: Response) => {
+router.post('/author', async (req: Request, res: Response) => {
     const author: type.Author = req.body
     promise.createAuthors(author).then((results) => {
-        res.send(`Author ${author.name} successfully created`).json(results).status(200)
+        res.json(results).status(200)
     }).catch(()=>{
         res.send('Error while creating an author').status(500)
     })
-  
 })
 
 // endpoint that allows a client to list created authors // 
-router.get('/', async (req: Request, res: Response) => {
+router.get('/author', async (req: Request, res: Response) => {
     promise.getAuthors.then((results) => {
         res.json(results).status(200)
     }).catch(() => {
@@ -29,7 +32,7 @@ router.get('/', async (req: Request, res: Response) => {
 router.post('/note', async (req: Request, res: Response) => {
     const note: type.Note = req.body
     promise.createNote(note).then((results) => {
-        res.send(`Note ${note.title} successfully created`).json(results).status(200)
+        res.json(results).status(200)
     }).catch(()=>{
         res.send('Error while creating an author').status(500)
     })
@@ -37,7 +40,7 @@ router.post('/note', async (req: Request, res: Response) => {
 
 // endpoint that allows a client to list created authors // 
 // expected parameter in params : author's id //
-router.get('/:id/note', async (req: Request, res: Response) => {
+router.get('/author/:id/note', async (req: Request, res: Response) => {
     const authorId : String = req.params.id
     promise.getAuthorsNotes(authorId).then((results) => {
         res.json(results).status(200)
--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import express, {Application} from "express"
 import bodyParser from "body-parser"
 import cors from "cors"
 import {note} from "./controller/index"
+
 const app : Application = express()
 
 app.use(cors()) 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', note)
+
 
 app.listen(4000, () => console.log('server running'))
